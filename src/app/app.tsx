007150c44875aba2f200a619182b9d7b9c164649
YuanhaoZhou <yzhou@stu.feitian.edu>
2023-12-07 15:31:33 -0500

    added input validation

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,14 +3,25 @@ import { Book, BookList } from "components/organisms/book-list";
 import { ChangeEvent, MouseEvent, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Outlet } from "react-router-dom";
+import { Formik, Field, Form, FormikHelpers } from 'formik';
+import { ErrorMessage } from "formik";
+import * as Yup from 'yup';
 
-{
-  //RENDER BOOKLIST+BOOKDETAILS
-}
+
+
+
 interface SearchResponse {
   numFound: number;
   docs: Book[];
 }
+
+const BookNameSchema = Yup.object().shape({
+  bookName: Yup.string()
+    .min(5, 'Book Name is Too Short!')
+    .max(50, 'Book Name is Too Long')
+    .required('Book Name is Required!')
+});
+
 
 function App() {
   const [bookName, setBookName] = useState("");
@@ -28,26 +39,45 @@ function App() {
     setFetchUri(`https://openlibrary.org/search.json?q=${encodedBookName}`);
   };
 
-
   return (
     <div className="flex min-h-[60vh] flex-col items-center justify-start gap-4 mt-20 text-center">
-      <div>
-        <input
+      <Formik
+        initialValues={{ bookName: '' }}
+        validationSchema={BookNameSchema}
+        onSubmit={(values, { resetForm }: FormikHelpers<{ bookName: string }>) => {
+          window.console.log(values);
+          resetForm();
+        }}
+      >
+  {({ values, handleChange, handleBlur }) => (
+    <Form>
+      <div className="flex-col justify-center items-center">
+        <Field
           type="text"
+          name="bookName"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Book Name"
-          required
-          value={bookName}
-          onChange={handleInputChange}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            handleChange(e);
+            handleInputChange(e);
+          }}
+          onBlur={handleBlur}
+          value={values.bookName}
+          style={{ width: '200px'}}
         />
+        <ErrorMessage name="bookName" component="div" className="text-red-500" />
       </div>
+
       <button
         type="button"
         onClick={handleClick}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Search Books
       </button>
+    </Form>
+  )}
+</Formik>
       <div className="flex flex-col ">
         <ErrorBoundary fallback={<div>Something went wrong</div>}>
           <Fetch<SearchResponse>
@@ -60,7 +90,7 @@ function App() {
             )}
           ></Fetch>
         </ErrorBoundary>
-        <div className="pl-4 mt-4 " style={{ width: '600px'}}>
+        <div className="pl-4 mt-4 " style={{ width: '600px' }}>
           <ErrorBoundary fallback={<div>Something went wrong</div>}>
             <Outlet />
           </ErrorBoundary>
